dbee1f13ea4b068615a94521cce98fd56e4c1767
zakels <kj.j.cha@gmail.com>
2018-06-09 14:20:14 -0400

    first commit

---
+++ zoom/src/Components/Main.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Title from './Title';
+
+class Main extends Component {
+    render() {
+        return  <div> 
+                    <Title title = 'Zoom'/>
+                </div>
+    }
+}
+
+export default Main
---
+++ zoom/src/index.js
@@ -0,0 +1,6 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Main from './Components/Main';
+
+
+ReactDOM.render(<Main/>, document.getElementById('root'));
